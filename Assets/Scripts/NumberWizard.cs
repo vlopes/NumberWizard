4e54fa92b2deb92013e1a90bc3699451dc561917
Vinicius <vinicius.ld@outlook.com>
2016-12-14 23:36:09 -0200

    Fim da primeira aula

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome to NumberWIzard");
+        print("Pick a number in your head but don't tell me");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
